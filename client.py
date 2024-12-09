03db8916cd7e09c07cc08013663d8799ac5dcef0
Andrew <elbroandrew@gmail.com>
2024-12-09 13:01:29 +1000

    encode decode msg

--- client.py
+++ client.py
@@ -3,11 +3,15 @@ import socket, sys
 
 def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.sendto(b'Hello World!', ('127.0.0.1', 5001))  # same as UDP server socket
+    msg = input("TEXT FOR SERVER: ")
+    msg = str.encode(msg) # to bytes
+    
+    udp_socket.sendto(msg, ('127.0.0.1', 5001))  # same as UDP server socket
 
-    udp_socket.settimeout(5)
+    udp_socket.settimeout(10)
 
     msg, server = udp_socket.recvfrom(1024)
+    msg = bytes.decode(msg)
     print("CLIENT RECIEVED MSG: ", msg)
 
     udp_socket.close()
--- server.py
+++ server.py
@@ -6,7 +6,8 @@ udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # dgram is UDP
 udp_socket.bind(("127.0.0.1", 5001))
 
 msg, addr = udp_socket.recvfrom(1024)
-print('Server UDP msg received: ', msg.upper())
+msg = bytes.decode(msg)
+print('Server UDP msg received: ', msg)
 
 udp_socket.sendto(b"HELLO FROM SERVER", addr)
 
