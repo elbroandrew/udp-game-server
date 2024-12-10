babf43df69d255474ef59149b340ca5a458431a2
Andrew <elbroandrew@gmail.com>
2024-12-10 15:01:46 +1000

    add: game window on client

--- client.py
+++ client.py
@@ -1,23 +1,63 @@
-import socket, sys
+import socket
+import sys
+import pygame
+import select
 
 
-def main():
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    msg = input("TEXT FOR SERVER: ")
-    msg = str.encode(msg) # to bytes
-    
-    udp_socket.sendto(msg, ('127.0.0.1', 5001))  # same as UDP server socket
+class Game:
+    def __init__(self):
+        
+        pygame.init()
 
-    udp_socket.settimeout(20)
+        W,H = 800,600
 
-    msg, server = udp_socket.recvfrom(1024)
-    msg = bytes.decode(msg)
-    print("CLIENT RECIEVED MSG: ", msg)
+        self.screen = pygame.display.set_mode((W,H))
+        self.clock = pygame.time.Clock()
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    udp_socket.close()
-    
+
+    def run(self):
+        
+        self.udp_socket.sendto(b'Client message', ('127.0.0.1', 5001))
+        self.udp_socket.setblocking(False)
+        inputs, outputs = [self.udp_socket], [self.udp_socket]
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.udp_socket.close()
+                    pygame.quit()
+                    sys.exit()
+                    
+            rlist, wlist, exlist = select.select(inputs, outputs, inputs)
+            for s in rlist:
+                if s is self.udp_socket:
+                    msg, server = self.udp_socket.recvfrom(1024)
+                    if msg:
+                        msg = bytes.decode(msg)
+                        print("CLIENT RECIEVED MSG: ", msg)
+                    else:
+                        print("SERVER CLOSED THE CONNECTION")
+                        sys.exit(1)
+                    
+
+            self.screen.fill((0, 0, 0))
+            pygame.display.flip()
+            self.clock.tick(60)
+            
+            
+            
+            
+    # def client_handle(self):
+    #     msg, server = self.udp_socket.recvfrom(1024)
+    #     msg = bytes.decode(msg)
+    #     print("CLIENT RECIEVED MSG: ", msg)
+
+
+
+
 if __name__ == "__main__":
     try:
-        main()
-    except socket.timeout:
+        Game().run()
+    except socket.error:
         sys.exit(1)
--- server.py
+++ server.py
@@ -4,7 +4,6 @@ import socket
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # dgram is UDP
 udp_socket.bind(("127.0.0.1", 5001))
 
-
 while True:
 
     print("waiting for data...")
